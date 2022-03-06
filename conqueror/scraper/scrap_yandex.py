0a3fb319cffbb9107a8a412dbb98b1eab610d893
kajtek <kacper.styslo@gmail.com>
2022-03-06 16:15:34 +0100

    Extract latitude & longitude for response.

--- conqueror/scraper/scrap_yandex.py
+++ conqueror/scraper/scrap_yandex.py
@@ -5,10 +5,22 @@ import requests
 from yandex_api_schema import YandexAPISchema
 
 
-def search_on_yandex(**kwargs: tp.Dict[str, str]) -> str:
-    """Search for places | business."""
+def search_on_yandex(**kwargs: tp.Dict[str, str]):
+    """Search for places | business.
+
+    :returns: Json with scraped information about points.
+    """
     url: str = "https://search-maps.yandex.ru/v1/?"
-    return requests.get(url=url, timeout=5, params={**kwargs}).text
+    return requests.get(url=url, timeout=5, params={**kwargs}).json()
 
 
-print(search_on_yandex(**dict(YandexAPISchema(text="test"))))
+def extract_point_coordinates(scraped_points) -> None:
+    for feature_key in scraped_points["features"]:
+        for geo_key, coordinates in feature_key["geometry"].items():
+            if isinstance(coordinates, list):
+                latitude, longitude = coordinates[0], coordinates[1]
+                print(f"Latitude: {latitude}, Longitude: {longitude}")
+
+
+scraped_points = search_on_yandex(**dict(YandexAPISchema(text="test")))
+extract_point_coordinates(scraped_points)
