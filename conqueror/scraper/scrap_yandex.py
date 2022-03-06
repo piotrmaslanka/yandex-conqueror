1276abb642c9264294e75bb078769d507707c7f1
kajtek <kacper.styslo@gmail.com>
2022-03-06 15:48:04 +0100

    Created schema for Yandex places API.

--- conqueror/scraper/scrap_yandex.py
+++ conqueror/scraper/scrap_yandex.py
@@ -1,31 +1,8 @@
 import typing as tp
+
 import requests
 
-# Provide yours API KEY
-API_KEY: str = "4331d83c-3f92-4d0f-bf93-823339e3e71f"
-
-search_for_place = {
-    "apikey": API_KEY,
-    "text": "cut,style",
-    "type": "biz",
-    "lang": "en_us",
-}
-
-# ll (search area center), spn(search area size)
-search_for_place_by_ll_spn = {
-    "apikey": API_KEY,
-    "text": "village of  Pozharishche",
-    "ll": "40.17248,60.594641",
-    "spn": "3.552069,2.400552",
-    "lang": "ru_RU",
-}
-
-search_for_business = {
-    "apikey": API_KEY,
-    "text": "Shear%20Pleasure",
-    "type": "biz",
-    "lang": "ru_RU",
-}
+from yandex_api_schema import YandexAPISchema
 
 
 def search_on_yandex(**kwargs: tp.Dict[str, str]) -> str:
@@ -34,4 +11,4 @@ def search_on_yandex(**kwargs: tp.Dict[str, str]) -> str:
     return requests.get(url=url, timeout=5, params={**kwargs}).text
 
 
-print(search_on_yandex(**search_for_place_by_ll_spn))
+print(search_on_yandex(**dict(YandexAPISchema(text="test"))))
