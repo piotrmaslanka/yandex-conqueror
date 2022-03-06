96f0ad5c22874357705938d6258e6eaf57bd98fb
kajtek <kacper.styslo@gmail.com>
2022-03-06 13:15:17 +0100

    Working.

--- conqueror/scraper/scrap_yandex.py
+++ conqueror/scraper/scrap_yandex.py
@@ -1,26 +1,17 @@
 import requests
 
 
-
-def search_for_businesses(*args, **kwargs) -> str:
-    """Search for businesses in the chosen location.
-    Provide 'text' arg with chosen location."""
+def search_for_places(**kwargs: str) -> str:
+    """Search for places by text(business desc)."""
     url: str = "https://search-maps.yandex.ru/v1/?"
-    r = requests.get(url, headers = {'User-agent': 'your bot 0.1'}, data={**kwargs}).text
-    # requests.post(url=url, *args, **kwargs).text
+    r = requests.get(url=url, timeout=5, params={**kwargs}).text
     return r
 
 
-
-print(search_for_businesses
+print(search_for_places
     (
-    apikey="pdct.1.1.20220306T104924Z.984ff829c71cc5d3.9f4187e0d018ecc1ea32cf94244b1df8941f2908",
-    text="Auto repair,Moscow, Smolenskaya ulitsa",
-    lang="ru_RU",
+    apikey="4331d83c-3f92-4d0f-bf93-823339e3e71f",
+    text="cut,style",
     type="biz",
-    verify=False
+    lang="en_us",
 ))
-
-
-
-# requests.exceptions.SSLError: HTTPSConnectionPool(host='search-maps.yandex.ru', port=443): Max retries exceeded with url: /v1/ (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1123)')))
