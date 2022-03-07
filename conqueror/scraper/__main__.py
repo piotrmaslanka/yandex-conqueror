c6fb50a2993e9a7ed53cebfd310d924e68e22ebe
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2022-03-07 08:17:14 +0100

    fix scraping mechanism

--- conqueror/scraper/__main__.py
+++ conqueror/scraper/__main__.py
@@ -5,6 +5,7 @@ from conqueror.scraper.pulse_coordinator import PulseCoordinator
 def run():
     pc = PulseCoordinator(OrdersObtainer())
     pc.start()
+    time.sleep(100000)
 
 
 if __name__ == '__main__':
