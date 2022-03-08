3405615d53a15cc4fd0153f4c08e1973486ea512
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2022-03-08 16:28:45 +0100

    removed message types 2-A and 2-B

--- conqueror/msg_generator/messages.py
+++ conqueror/msg_generator/messages.py
@@ -4,11 +4,7 @@ from .definitions import get_messages, get_messages_backend
 
 
 def get_msg_using_provider(provider):
-    random_msg = random.randint(1, 10)
-    if random_msg < 7:
-        return str(random.choice(provider('1-A')))
-    else:
-        return str(random.choice(provider('2-A'))+'\n'+ str(random.choice(provider('2-B'))))
+    return str(random.choice(provider('1-A')))
 
 
 def generate_message() -> str:
