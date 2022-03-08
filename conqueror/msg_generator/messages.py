011bbabff7198d5689c11a5e47fbb967ac4dbd5e
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2022-03-08 14:33:15 +0100

    refactor the message provider a bit

--- conqueror/msg_generator/messages.py
+++ conqueror/msg_generator/messages.py
@@ -3,25 +3,17 @@ import random
 from .definitions import get_messages, get_messages_backend
 
 
-def generate_message() -> str:
+def get_msg_using_provider(provider):
     random_msg = random.randint(1, 10)
     if random_msg < 7:
-        return str(random.choice(get_messages('1-A')))
+        return str(random.choice(provider('1-A')))
     else:
-        return str(random.choice(get_messages('2-A'))+'\n'+ str(random.choice(get_messages('2-B'))))
-    # else:
-    #     return str(random.choice(get_messages('3-A'))) + '\n' + str(random.choice(get_messages('3-B')))\
-    #          + '\n' + str(random.choice(get_messages('3-C'))) + '\n' + str(random.choice(get_messages('3-D'))) + '\n' \
-    #          + str(random.choice(get_messages('3-E')))
+        return str(random.choice(provider('2-A'))+'\n'+ str(random.choice(provider('2-B'))))
+
+
+def generate_message() -> str:
+    return get_msg_using_provider(get_messages)
 
 
 def generate_message_client() -> str:
-    random_msg = random.randint(1, 10)
-    if random_msg < 7:
-        return str(random.choice(get_messages_backend('1-A')))
-    else:
-        return str(random.choice(get_messages_backend('2-A'))+'\n'+ str(random.choice(get_messages_backend('2-B'))))
-    # else:
-    #     return str(random.choice(get_messages_backend('3-A'))) + '\n' + str(random.choice(get_messages_backend('3-B')))\
-    #          + '\n' + str(random.choice(get_messages_backend('3-C'))) + '\n' + str(random.choice(get_messages_backend('3-D'))) + '\n' \
-    #          + str(random.choice(get_messages_backend('3-E')))
+    return get_msg_using_provider(get_messages_backend)
