1a3836e516aa16e3fe51580d2078ee02205b02f8
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2022-03-10 08:41:59 +0100

    hotfix 1.1

--- conqueror/msg_generator/messages.py
+++ conqueror/msg_generator/messages.py
@@ -7,7 +7,7 @@ def get_msg_using_provider(provider):
     if random.randint(1, 5) <= 3:
         return str(random.choice(provider('1-A')))
     else:
-        return str(random.choice(provider('2-A')+' '+provider('2-B')))
+        return str(random.choice(provider('2-A')))+' '+str(random.choice(provider('2-B')))
 
 
 def generate_message() -> str:
