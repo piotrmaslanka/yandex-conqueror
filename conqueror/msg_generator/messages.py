8c1d4b23d75e027b742aee9157e0bd6cd5d1d994
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2022-03-10 08:35:35 +0100

    add messages, v0.7 2-A message types

--- conqueror/msg_generator/messages.py
+++ conqueror/msg_generator/messages.py
@@ -4,7 +4,10 @@ from .definitions import get_messages, get_messages_backend
 
 
 def get_msg_using_provider(provider):
-    return str(random.choice(provider('1-A')))
+    if random.randint(1, 5) <= 3:
+        return str(random.choice(provider('1-A')))
+    else:
+        return str(random.choice(provider('2-A')+' '+provider('2-B')))
 
 
 def generate_message() -> str:
